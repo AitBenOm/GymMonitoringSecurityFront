b621b1023d1df1f1c6896c43e0c38136ede64fe8
AitBenOm <omar.benaissa@outlook.com>
2018-03-06 17:27:32 +0100

    upload image

--- src/app/user/user-edit/user-edit.component.ts
+++ src/app/user/user-edit/user-edit.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
 import {UserService} from '../user.service';
 import {UserModel} from '../user.model';
+import {HttpEventType, HttpResponse} from "@angular/common/http";
 
 @Component({
   selector: 'app-user-edit',
@@ -8,14 +9,54 @@ import {UserModel} from '../user.model';
   styleUrls: ['./user-edit.component.css']
 })
 export class UserEditComponent implements OnInit {
-  constructor(private userService: UserService) { }
-  user: UserModel;
-  imageProfil: Blob;
-  ngOnInit() {
-    this.user=this.userService.getUserFromToken();
-  }
-  show(){
-    console.log(this.imageProfil);
+  constructor(private userService: UserService) {
   }
 
+  user: UserModel;
+  selectedFiles: FileList;
+  currentFileUpload: File;
+  progress: { percentage: number } = { percentage: 0 };
+  @ViewChild('fileInput') fileInput;
+
+  ngOnInit() {
+    this.user = this.userService.getUserFromToken();
+  }
+
+  // upload1() {
+  //   this.progress.percentage = 0;
+  //   this.currentFileUpload = this.selectedFiles.item(0);
+  //
+  //   const fileBrowser = this.fileInput.nativeElement;
+  //   if (fileBrowser.files && fileBrowser.files[0]) {
+  //     const formData = new FormData();
+  //     formData.append("image", fileBrowser.files[0]);
+  //     // this.projectService.upload(formData, this.project.id).subscribe(res => {
+  //     console.log(formData.get('image'));
+  //     // do stuff w/my uploaded file
+  //     this.userService.saveFile(formData).subscribe(
+  //       (data: any) => {
+  //         console.log(data);
+  //       }
+  //     );
+  //   }
+  // }
+
+  upload() {
+    this.progress.percentage = 0;
+
+    this.currentFileUpload = this.selectedFiles.item(0);
+    this.userService.pushFileToStorage(this.currentFileUpload).subscribe(
+      (event: any) => {
+      if (event.type === HttpEventType.UploadProgress) {
+        this.progress.percentage = Math.round(100 * event.loaded / event.total);
+      } else if (event instanceof HttpResponse) {
+        console.log('File is completely uploaded!');
+      }
+    });
+
+    this.selectedFiles = undefined;
+  }
+  selectFile(event) {
+    this.selectedFiles = event.target.files;
+  }
 }
--- src/app/user/user.service.ts
+++ src/app/user/user.service.ts
@@ -1,32 +1,72 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {UserModel} from "./user.model";
 import {JwtHelper} from "angular2-jwt";
 import {AuthService} from "../Auth/auth.service";
 import {Router} from '@angular/router';
-import {HttpClient} from '@angular/common/http';
+import {HttpClient, HttpEvent, HttpHeaders, HttpRequest} from '@angular/common/http';
+import {Observable} from "rxjs/Observable";
 
 @Injectable()
 export class UserService {
 
-  isAuthenticated: boolean= false;
+  isAuthenticated: boolean = false;
 
-   user : UserModel;
-  constructor(private http: HttpClient, private authService: AuthService,  private router: Router) {
+  user: UserModel;
+
+  constructor(private http: HttpClient, private authService: AuthService, private router: Router) {
   }
 
   jwtHelper: JwtHelper = new JwtHelper();
 
 
-getUserFromToken(){
+  getUserFromToken() {
 
-    this.user=this.jwtHelper.decodeToken( this.authService.getToken()).myUser;
+    this.user = this.jwtHelper.decodeToken(this.authService.getToken()).myUser;
     console.log(this.user);
     return this.user;
-}
+  }
+
   getImage(idProgram: number) {
-  console.log('getimage');
+    console.log('getimage');
     return this.http.get('http://localhost:8080/Programs/imgProfil?id=' + idProgram, {
       headers: this.authService.getHeaders()
     });
   }
+
+  //
+  // saveFile(formData: FormData) {
+  //   return this.http.post('http://localhost:8080/Users/upload?file=' + formData, {
+  //     headers: this.authService.getHeaders()
+  //     // .set('Content-Type','image/png')
+  //   });
+  // }
+  saveFile(formData: File) {
+    const MyHeaders = new HttpHeaders();
+    return this.http.post('http://localhost:8080/Users/saveFile', formData, {
+      headers: MyHeaders.set('authorization', 'Bearer ' + this.authService.getToken()),
+      reportProgress: true,
+      responseType: 'text'
+      // .set('Content-Type','image/png')
+    });
+  }
+
+  pushFileToStorage(file: File){
+    let formdata: FormData = new FormData();
+    const MyHeaders = new HttpHeaders();
+
+    formdata.append('file', file);
+    return this.http.post('http://localhost:8080/Users/saveFile', formdata, {
+      headers: MyHeaders.set('authorization', 'Bearer ' + this.authService.getToken()),
+      reportProgress: true,
+      responseType: 'text'
+      // .set('Content-Type','image/png')
+    });
+    // const req = new HttpRequest('POST', '/Users/saveFile', formdata, {
+    //   reportProgress: true,
+    //   responseType: 'text'
+    // });
+
+
+  }
+
 }
