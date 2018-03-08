e20ad28451b7a3d49aad793fc0fa3f4f7cbc9d83
AitBenOm <omar.benaissa@outlook.com>
2018-03-08 11:02:42 +0100

    upload image

--- src/app/user/user-profil/user-profil.component.ts
+++ src/app/user/user-profil/user-profil.component.ts
@@ -1,17 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 import {UserService} from '../user.service';
 import {UserModel} from '../user.model';
-
+import {DomSanitizer} from "@angular/platform-browser";
+interface JsonImage{
+  name: string;
+  content: string;
+}
 @Component({
   selector: 'app-user-profil',
   templateUrl: './user-profil.component.html',
   styleUrls: ['./user-profil.component.css']
 })
 export class UserProfilComponent implements OnInit {
-  constructor(private userService: UserService) { }
-  user: UserModel;
+  constructor(private userService: UserService, private sanitizer: DomSanitizer) { }
+  user: UserModel;  avatar: any;
+  private readonly imageType : string = 'data:image/PNG;base64,';
   ngOnInit() {
     this.user=this.userService.getUserFromToken();
+    this.userService.getFile().subscribe(
+      (data: JsonImage) => {
+        //  console.log(data.content);
+        this.avatar = this.sanitizer.bypassSecurityTrustUrl(this.imageType+data.content);
+      }
+    );
   }
 
 }
--- src/app/user/user.model.ts
+++ src/app/user/user.model.ts
@@ -5,6 +5,7 @@ firstName: string;
 lastName: string;
 email: string;
 pwd: string;
+avatarPath: string;
 
 
   constructor(firstName: string, lastName: string, email: string, pwd: string) {
